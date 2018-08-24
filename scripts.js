9404c4c47773c9c41973bde50ee8bf271e76f1c5
Jessica Duell <duellrdh@gmail.com>
2018-08-23 23:07:28 -0400

    Song will play once button is clicked once

--- scripts.js
+++ scripts.js
@@ -8,14 +8,15 @@ btn.innerText = "Sing!";
 body.appendChild(btn);
 
 //Goes through each name in the array friends and console.log a song inputting each name in the song once the button Sing! is clicked once
-
-for (let i = 0; i < friends.length; i++){
-    let name = friends[i];
-    for (let x = 99; x > 0; x--){
-        if (x > 1){
-        console.log(x + " lines of code in the file, " + x  + " lines of code; " + name + " strikes one out, clears it all out, " + (x-1) + " lines of code in the file");
-        } else {
-            console.log(x + " lines of code in the file, " + x  + " lines of code; " + name + " strikes one out, clears it all out, no more lines of code in the file");
+btn.addEventListener("click", function () {
+    for (let i = 0; i < friends.length; i++) {
+        let name = friends[i];
+        for (let x = 99; x > 0; x--) {
+            if (x > 1) {
+                console.log(x + " lines of code in the file, " + x + " lines of code; " + name + " strikes one out, clears it all out, " + (x - 1) + " lines of code in the file");
+            } else {
+                console.log(x + " lines of code in the file, " + x + " lines of code; " + name + " strikes one out, clears it all out, no more lines of code in the file");
+            }
         }
-    }
-};
+    };
+})
