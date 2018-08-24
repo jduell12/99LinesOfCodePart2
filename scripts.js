a041aeb329aaed2c48b444fff5e92099179b5b97
Jessica Duell <duellrdh@gmail.com>
2018-08-23 23:04:53 -0400

    added button

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 //Array that holds 5 friends' names
 let friends = ["Alex", "Jeremy", "Andrea", "Michelle", "Sam"];
 
-//Goes through each name in the array friends and console.log a song inputting each name in the song
+//Puts button in the body of the HTML that says Sing!
+let body = document.body;
+let btn = document.createElement("button");
+btn.innerText = "Sing!";
+body.appendChild(btn);
+
+//Goes through each name in the array friends and console.log a song inputting each name in the song once the button Sing! is clicked once
+
 for (let i = 0; i < friends.length; i++){
     let name = friends[i];
     for (let x = 99; x > 0; x--){
