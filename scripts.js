c51c0c3b7f5ad08b26231456140a5f78e8bdf68a
Jessica Duell <duellrdh@gmail.com>
2018-08-24 22:20:28 -0400

    added headers to divs

--- scripts.js
+++ scripts.js
@@ -30,9 +30,29 @@ body.appendChild(samDiv);
 
 //Creating a header that lists the friend in their respective div 
 let h3 = document.createElement('h3');
-let h3Txt = document.createTextNode("Alex is ready to sing");
+let h3Txt = document.createTextNode("Alex is ready to sing. Just click the button.");
 h3.appendChild(h3Txt);
 alexDiv.appendChild(h3);
+
+let jH3 = document.createElement('h3');
+let jTxt = document.createTextNode("Jeremy will show you his amazing singing voice! Just click the button");
+jH3.appendChild(jTxt);
+jeremyDiv.appendChild(jH3);
+
+let aH3 = document.createElement('h3');
+let aTxt = document.createTextNode("Andrea is ready to sing. Just click the button.");
+aH3.appendChild(aTxt);
+andreaDiv.appendChild(aH3);
+
+let mH3 = document.createElement('h3');
+let mTxt = document.createTextNode("Michelle is ready. Just click the button.");
+mH3.appendChild(mTxt);
+michelleDiv.appendChild(mH3);
+
+let sH3 = document.createElement('h3');
+let sTxt = document.createTextNode("Sam can't sing very well. Just click the button.");
+sH3.appendChild(sTxt);
+samDiv.appendChild(sH3);
 
 //Goes through each name in the array friends and console.log a song inputting each name in the song once the button Sing! is clicked once
 btn.addEventListener("click", function () {
