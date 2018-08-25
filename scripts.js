8de7631b0c75c3dbda0035cc54ae12bed5750a9c
Jessica Duell <duellrdh@gmail.com>
2018-08-24 22:38:00 -0400

    completed

--- scripts.js
+++ scripts.js
@@ -7,55 +7,58 @@ let btn = document.createElement("button");
 btn.innerText = "Sing!";
 body.appendChild(btn);
 
-//Creating a div with class name of friend for each friend
-let alexDiv = document.createElement('div');
-alexDiv.className = 'friend';
-body.appendChild(alexDiv);
 
-let jeremyDiv = document.createElement('div');
-jeremyDiv.className = 'friend';
-body.appendChild(jeremyDiv);
-
-let andreaDiv = document.createElement('div');
-andreaDiv.className = 'friend';
-body.appendChild(andreaDiv);
-
-let michelleDiv = document.createElement('div');
-michelleDiv.className = 'friend';
-body.appendChild(michelleDiv);
-
-let samDiv = document.createElement('div');
-samDiv.className = 'friend';
-body.appendChild(samDiv);
-
-//Creating a header that lists the friend in their respective div 
-let h3 = document.createElement('h3');
-let h3Txt = document.createTextNode("Alex is ready to sing. Just click the button.");
-h3.appendChild(h3Txt);
-alexDiv.appendChild(h3);
-
-let jH3 = document.createElement('h3');
-let jTxt = document.createTextNode("Jeremy will show you his amazing singing voice! Just click the button");
-jH3.appendChild(jTxt);
-jeremyDiv.appendChild(jH3);
-
-let aH3 = document.createElement('h3');
-let aTxt = document.createTextNode("Andrea is ready to sing. Just click the button.");
-aH3.appendChild(aTxt);
-andreaDiv.appendChild(aH3);
-
-let mH3 = document.createElement('h3');
-let mTxt = document.createTextNode("Michelle is ready. Just click the button.");
-mH3.appendChild(mTxt);
-michelleDiv.appendChild(mH3);
-
-let sH3 = document.createElement('h3');
-let sTxt = document.createTextNode("Sam can't sing very well. Just click the button.");
-sH3.appendChild(sTxt);
-samDiv.appendChild(sH3);
 
 //Goes through each name in the array friends and console.log a song inputting each name in the song once the button Sing! is clicked once
 btn.addEventListener("click", function () {
+
+    //Creating a div with class name of friend for each friend
+    let alexDiv = document.createElement('div');
+    alexDiv.className = 'friend';
+    body.appendChild(alexDiv);
+
+    let jeremyDiv = document.createElement('div');
+    jeremyDiv.className = 'friend';
+    body.appendChild(jeremyDiv);
+
+    let andreaDiv = document.createElement('div');
+    andreaDiv.className = 'friend';
+    body.appendChild(andreaDiv);
+
+    let michelleDiv = document.createElement('div');
+    michelleDiv.className = 'friend';
+    body.appendChild(michelleDiv);
+
+    let samDiv = document.createElement('div');
+    samDiv.className = 'friend';
+    body.appendChild(samDiv);
+
+    //Creating a header that lists the friend in their respective div 
+    let h3 = document.createElement('h3');
+    let h3Txt = document.createTextNode("Alex is ready to sing.");
+    h3.appendChild(h3Txt);
+    alexDiv.appendChild(h3);
+
+    let jH3 = document.createElement('h3');
+    let jTxt = document.createTextNode("Jeremy will show you his amazing singing voice!");
+    jH3.appendChild(jTxt);
+    jeremyDiv.appendChild(jH3);
+
+    let aH3 = document.createElement('h3');
+    let aTxt = document.createTextNode("Andrea is ready to sing.");
+    aH3.appendChild(aTxt);
+    andreaDiv.appendChild(aH3);
+
+    let mH3 = document.createElement('h3');
+    let mTxt = document.createTextNode("Michelle is ready.");
+    mH3.appendChild(mTxt);
+    michelleDiv.appendChild(mH3);
+
+    let sH3 = document.createElement('h3');
+    let sTxt = document.createTextNode("Sam can't sing very well.");
+    sH3.appendChild(sTxt);
+    samDiv.appendChild(sH3);
+
     for (let i = 0; i < friends.length; i++) {
         let name = friends[i];
         for (let x = 99; x > 0; x--) {
