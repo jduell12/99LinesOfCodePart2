7ed9497939c1786065fcd939d435b37a88fb9035
Jessica Duell <duellrdh@gmail.com>
2018-08-24 22:07:31 -0400

    added H3 using DOM

--- scripts.js
+++ scripts.js
@@ -7,6 +7,17 @@ let btn = document.createElement("button");
 btn.innerText = "Sing!";
 body.appendChild(btn);
 
+//Creating a div with class name of friend
+let div = document.createElement('div');
+div.className = 'friend';
+body.appendChild(div);
+
+//Creating a header that lists the friends
+let h3 = document.createElement('h3');
+let h3Txt = document.createTextNode("Alex, Jeremy, Andrea, Michelle and Sam will sing a song.");
+h3.appendChild(h3Txt);
+div.appendChild(h3);
+
 //Goes through each name in the array friends and console.log a song inputting each name in the song once the button Sing! is clicked once
 btn.addEventListener("click", function () {
     for (let i = 0; i < friends.length; i++) {
@@ -18,5 +29,5 @@ btn.addEventListener("click", function () {
                 console.log(x + " lines of code in the file, " + x + " lines of code; " + name + " strikes one out, clears it all out, no more lines of code in the file");
             }
         }
-    };
-})
+    }
+});
