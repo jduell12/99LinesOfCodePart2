11ddf0b30c328c6ba56916c140b7214c256ecaea
Jessica Duell <duellrdh@gmail.com>
2018-08-24 22:15:43 -0400

    created div per friend

--- scripts.js
+++ scripts.js
@@ -7,16 +7,32 @@ let btn = document.createElement("button");
 btn.innerText = "Sing!";
 body.appendChild(btn);
 
-//Creating a div with class name of friend
-let div = document.createElement('div');
-div.className = 'friend';
-body.appendChild(div);
+//Creating a div with class name of friend for each friend
+let alexDiv = document.createElement('div');
+alexDiv.className = 'friend';
+body.appendChild(alexDiv);
 
-//Creating a header that lists the friends
+let jeremyDiv = document.createElement('div');
+jeremyDiv.className = 'friend';
+body.appendChild(jeremyDiv);
+
+let andreaDiv = document.createElement('div');
+andreaDiv.className = 'friend';
+body.appendChild(andreaDiv);
+
+let michelleDiv = document.createElement('div');
+michelleDiv.className = 'friend';
+body.appendChild(michelleDiv);
+
+let samDiv = document.createElement('div');
+samDiv.className = 'friend';
+body.appendChild(samDiv);
+
+//Creating a header that lists the friend in their respective div 
 let h3 = document.createElement('h3');
-let h3Txt = document.createTextNode("Alex, Jeremy, Andrea, Michelle and Sam will sing a song.");
+let h3Txt = document.createTextNode("Alex is ready to sing");
 h3.appendChild(h3Txt);
-div.appendChild(h3);
+alexDiv.appendChild(h3);
 
 //Goes through each name in the array friends and console.log a song inputting each name in the song once the button Sing! is clicked once
 btn.addEventListener("click", function () {
